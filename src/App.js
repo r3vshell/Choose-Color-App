09bd7fca887ac333388c2785a52475fdda2f4b01
Masus <masus2010@gmail.com>
2023-09-11 21:56:54 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Site Under Construction, come back later!!!
         </a>
       </header>
     </div>
